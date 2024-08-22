4af9a1090920263637a874aef50e3af5118f4b0b
Kyle Grymonprez <>
2024-08-22 10:08:03 -0700

    more testing

--- xLights/sequencer/MainSequencer.cpp
+++ xLights/sequencer/MainSequencer.cpp
@@ -238,8 +238,8 @@ MainSequencer::MainSequencer(wxWindow* parent, bool smallWaveform, wxWindowID id
     Create(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL|wxWANTS_CHARS, _T("wxID_ANY"));
     FlexGridSizer1 = new wxFlexGridSizer(3, 3, 0, 0);
     FlexGridSizer1->AddGrowableCol(0);
+    FlexGridSizer1->AddGrowableCol(1);
     FlexGridSizer1->AddGrowableRow(1);
-    FlexGridSizer1->AddGrowableRow(2);
     FlexGridSizer2 = new wxFlexGridSizer(0, 1, 0, 0);
     FlexGridSizer2->AddGrowableCol(0);
     StaticText1 = new wxStaticText(this, wxID_ANY, _("View:"), wxDefaultPosition, wxDefaultSize, 0, _T("wxID_ANY"));
@@ -274,7 +274,7 @@ MainSequencer::MainSequencer(wxWindow* parent, bool smallWaveform, wxWindowID id
     CheckBox_SuspendRender->SetValue(false);
     FlexGridSizer1->Add(CheckBox_SuspendRender, 1, wxALL|wxEXPAND, 0);
     FlexGridSizer3 = new wxFlexGridSizer(1, 1, 0, 0);
-    FlexGridSizer3->AddGrowableCol(1);
+    FlexGridSizer3->AddGrowableCol(0);
     FlexGridSizer3->AddGrowableRow(0);
     ScrollBarEffectsHorizontal = new wxScrollBar(this, ID_SCROLLBAR_EFFECT_GRID_HORZ, wxDefaultPosition, wxDefaultSize, wxSB_HORIZONTAL|wxALWAYS_SHOW_SB, wxDefaultValidator, _T("ID_SCROLLBAR_EFFECT_GRID_HORZ"));
     ScrollBarEffectsHorizontal->SetScrollbar(0, 1, 100, 1);
